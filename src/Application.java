c5bfbc988a1c0d1ae3c9b97c6f82f68533c34709
manueltov <manuel@tovar.pt>
2019-02-18 15:14:03 +0000

    one more thing!! and some editions on the mail

--- src/Application.java
+++ src/Application.java
@@ -2,4 +2,9 @@ public class Application {
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
+    /*
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+    */
 }
---
+++ src/oneMoreThing.java
@@ -0,0 +1,5 @@
+public class oneMoreThing {
+    public static void main(String[] args) {
+        System.out.println("One more thing...");
+    }
+}
